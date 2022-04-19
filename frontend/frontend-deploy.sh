b64f63651a76b040be6cb823f3318f30ffb000d3
yuki.isoya23 <yuki.isoya23@yandex.ru>
2022-04-19 15:26:28 +0300

    add 'sudo' mv sausage-store

--- frontend/frontend-deploy.sh
+++ frontend/frontend-deploy.sh
@@ -7,7 +7,7 @@ sudo rm -f /var/www-data/dist/frontend/*||true
 #Переносим артефакт в нужную папку
 curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o frontend.tar.gz $NEXUS_REPO_URL/sausage-store-dvorov-ivan-frontend/sausage-store/$VERSION/sausage-store-$VERSION.tar.gz
 tar -xvf frontend.tar.gz sausage-store-$VERSION #Распаковка архива
-mv sausage-store-$VERSION/public_html/* /var/www-data/dist/frontend/||true #"jar||true" говорит, если команда обвалится — продолжай
+sudo mv sausage-store-$VERSION/public_html/* /var/www-data/dist/frontend/||true #"jar||true" говорит, если команда обвалится — продолжай
 rm -rf 'sausage-store-1'* #Удаляем папку sausage-store
 #Меняем владельца директории с root на front-user
 sudo chown -R front-user:front-user /var/www-data/dist/frontend/
