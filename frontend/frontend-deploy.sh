b39a29ff5fdef9eaaf622bc44af7808ec15a671a
yuki.isoya23 <yuki.isoya23@yandex.ru>
2022-06-06 14:59:17 +0300

    try to add frontend docker

--- frontend/frontend-deploy.sh
+++ frontend/frontend-deploy.sh
@@ -1,17 +1,12 @@
-#! /bin/bash
-#Если свалится одна из команд, рухнет и весь скрипт
-set -xe
-#Перезаливаем дескриптор сервиса frontend на ВМ для деплоя
-sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
-sudo rm -f /var/www-data/dist/frontend/*||true
-#Переносим артефакт в нужную папку
-curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o frontend.tar.gz $NEXUS_REPO_URL/sausage-store-dvorov-ivan-frontend/sausage-store/$VERSION/sausage-store-$VERSION.tar.gz
-tar -xvf frontend.tar.gz sausage-store-$VERSION #Распаковка архива
-sudo mv sausage-store-$VERSION/public_html/* /var/www-data/dist/frontend/||true #"jar||true" говорит, если команда обвалится — продолжай
-rm -rf 'sausage-store-1'* #Удаляем папку sausage-store
-#Меняем владельца директории с root на front-user
-sudo chown -R front-user:front-user /var/www-data/dist/frontend/
-#Обновляем конфиг systemd с помощью рестарта
-sudo systemctl daemon-reload
-#Перезапускаем сервис frontend сосисочной
-sudo systemctl restart sausage-store-frontend.service
+#!/bin/bash
+docker network create -d bridge sausage_network || true
+gitlab.praktikum-services.ru:5050/yuki.isoya23/sausage-store/sausage-frontend:latest
+docker stop frontend || true
+docker rm frontend || true
+set -e
+docker run -d --name frontend \
+    --network=sausage_network \
+	-p 3543:3543 \
+    --restart always \
+    --pull always \
+	gitlab.praktikum-services.ru:5050/yuki.isoya23/sausage-store/sausage-frontend:latest
